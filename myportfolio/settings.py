81396ecc531815e878749112f992035dc59ffe65
ChinnakotlaSreeharsha <chinnakotlasreeharsha6272@gmail.com>
2025-06-24 17:08:26 +0530

    fix: added true

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['main.onrender.com', 'myportfolio-i3gd.onrender.com', 'localhost', '127.0.0.1']
 
