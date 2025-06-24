e1b7aad246af69890ba342436f1c695220c72823
ChinnakotlaSreeharsha <chinnakotlasreeharsha6272@gmail.com>
2025-06-24 14:22:44 +0530

    fix: added missing Path import

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -116,6 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 STATIC_URL = '/static/'  # already present
 
