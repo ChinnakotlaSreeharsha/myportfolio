fab96e8b2e7e406db4712ea17f2b96a9b85a3a48
ChinnakotlaSreeharsha <chinnakotlasreeharsha6272@gmail.com>
2025-06-24 14:37:17 +0530

    fix: added correct domain to ALLOWED_HOSTS

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['main.onrender.com']
+ALLOWED_HOSTS = ['main.onrender.com', 'myportfolio-i3gd.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
