5176b5af90f84c7b19bb025e737ab8dab8b18acd
ChinnakotlaSreeharsha <chinnakotlasreeharsha6272@gmail.com>
2025-06-24 17:15:22 +0530

    fix: added host user

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -114,8 +114,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chinnakotlasreeharsha6272@gmail.com'
-EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASSWORD'  # Replace with actual App Password or use env var
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Caching (adjust location for Unix-based systems)
