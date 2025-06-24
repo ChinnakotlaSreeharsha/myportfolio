b4bb382910286c51c90ddb4ed0590b3b664e3b2e
ChinnakotlaSreeharsha <chinnakotlasreeharsha6272@gmail.com>
2025-06-24 14:19:14 +0530

    fix: added environment variable setup

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -140,7 +140,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'chinnakotlasreeharsha6272@gmail.com'
-EMAIL_HOST_PASSWORD = 'zzcr vucl hbjv vhxl'  # Use App Password for Gmail
+EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASSWORD'  # Use App Password for Gmail
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
